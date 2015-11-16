1323538a76d8c0bcfead5737408e9255c181d49c
Christian Angel Galamay <christian@redtonic.net>
2015-11-16 12:34:33 +0800

    fix layout (partial)

--- app/assets/javascripts/template/template.js
+++ app/assets/javascripts/template/template.js
@@ -1,6 +1,6 @@
 //= require template/modernizr.js
 //= require template/jquery-migrate-1.2.1.min.js
-//= requrie template/jquery.flexslider.js
+//= require template/jquery.flexslider.js
 //= require template/waypoints.js
 //= require template/jquery.fittext.js
 //= require template/magnific-popup.js
